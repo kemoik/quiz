bd98cf5e17fe1b055b5ee10a62f5e0116113f31d
caleb <ck.caleb17@gmail.com>
2019-02-22 17:04:14 +0300

    add javascript test headers to the page

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,19 @@
-// Business logic://
-
-
 $(document).ready(function() {
-  $("form#names").submit(function(event) {
+    $("form#names").submit(function(event) {
     event.preventDefault();
-  var names= $("input#name1").val();
-  $("#name").text(names);
-  $(".names").show();
-});
-});
-$(document).ready(function() {
-  $("#font").submit(function(event) {
-    event.preventDefault();
-  var q1= parseInT(("input:[name=question1]:checked").val())$;
-  $("#display").text(q1);
-});
+    var names= $("input#name1").val();
+    $("#name").text(names);
+    $(".names").show();
+  });
+
+    $("form#font").submit(function(event) {
+      event.preventDefault();
+      var qst1 = parseInt($("input:radio[name=question1]:checked").val());
+      var qst2 = parseInt($("input:radio[name=question2]:checked").val());
+      var qst3 = parseInt($("input:radio[name=question3]:checked").val());
+      var qst4 = parseInt($("input:radio[name=question4]:checked").val());
+      var qst5 = parseInt($("input:radio[name=question5]:checked").val());
+      var results =(qst1 + qst2 + qst3 + qst4 + qst5 );
+      $("#display").text(results)
+    });
 });
